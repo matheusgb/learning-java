fed6eb94a82c56d5cc67c070392feab34b4f59ba
matheusgb <matheusgbr98@gmail.com>
2022-06-28 15:46:24 -0300

    learning casting

---
+++ intdouble.java
@@ -0,0 +1,13 @@
+public class intdouble {
+
+  public static void main(String[] args) {
+    int a, b;
+    double resultado;
+
+    a = 5;
+    b = 2;
+    resultado = (double) a / b; // Se não adicionar o double, o resultado será 2.0 pois a e b são inteiros.
+
+    System.out.println(resultado);
+  }
+}
